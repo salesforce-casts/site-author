6dcee300c66a58018ab0adbd898f8974a038302f
salesfore-casts <teja@salesforcecasts.com>
2023-03-22 01:45:40 +0530

    intial commit

---
+++ resources/views/site-author.blade.php
@@ -0,0 +1,4 @@
+
+        <div class="bg-indigo-500">
+            <p class="text-2xl text-green-500">The is Site Author...</p>
+        </div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    </head>
+    <body class="antialiased">
+        <div class="flex flex-col w-screen h-screen relative overflow-hidden">
+            @include('rightsidebar')
+        </div>
+    </body>
+</html>
