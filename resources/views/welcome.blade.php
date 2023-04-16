61fcc23824543c0466c866628be84e1de2794e73
salesfore-casts <teja@salesforcecasts.com>
2023-04-16 22:01:56 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
          <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
-    <body class="antialiased h-full>
+    <body class="antialiased h-full">
 
         @include('header')
             
